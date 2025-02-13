06aacc9d9b9ee3383b1a5fab817d8a41c086f4f3
WojKlo <klos.wojciech001@gmail.com>
2025-02-13 13:35:20 +0100

    Change background and button colors

--- CV/script.js
+++ CV/script.js
@@ -4,46 +4,31 @@ var exp_button = document.getElementById("experience");
 var contact_button = document.getElementById("call-me");
 
 about_button.onclick = function()
-{
-    if (location.href == "http://127.0.0.1:5500/CV/index.html")
-    {
-        return 0;
-    }
-    else
+{  
+    if (location.href !== "http://127.0.0.1:5500/CV/index.html")
     {
         location.href = "http://127.0.0.1:5500/CV/index.html";
     }
 }
 projects_button.onclick = function()
 {
-    if (location.href == "http://127.0.0.1:5500/CV/projects.html")
-    {
-        return 0;
-    }
-    else
+    if (location.href !== "http://127.0.0.1:5500/CV/projects.html")
     {
         location.href = "http://127.0.0.1:5500/CV/projects.html";
     }
 }
 exp_button.onclick = function()
 {
-    if (location.href == "http://127.0.0.1:5500/CV/experience.html")
-    {
-        return 0;
-    }
-    else
+    if (location.href !== "http://127.0.0.1:5500/CV/experience.html")
     {
         location.href = "http://127.0.0.1:5500/CV/experience.html";
     }
 }
 contact_button.onclick = function()
 {
-    if (location.href == "http://127.0.0.1:5500/CV/contact.html")
-    {
-        return 0;
-    }
-    else
+    if (location.href !== "http://127.0.0.1:5500/CV/contact.html")
     {
         location.href = "http://127.0.0.1:5500/CV/contact.html";
     }
+    
 }
